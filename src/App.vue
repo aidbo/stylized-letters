3ae373b83bade4c69449dce8ca931025f1f643fd
Zheng Bang-Bo <zhengbangbo@hotmail.com>
2023-07-25 13:01:03 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Stylized Letters',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Browse through those stylized letters' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
--- vite.config.ts
+++ vite.config.ts
@@ -96,10 +96,10 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      includeAssets: ['favicon.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'Stylized Letters',
+        short_name: 'SL',
         theme_color: '#ffffff',
         icons: [
           {
