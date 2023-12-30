e9492636bde650031949e40a001c6d4a416af727
Zheng Bang-Bo <zhengbangbo@hotmail.com>
2023-12-30 19:35:31 +0800

    chore: update style and layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -40,36 +40,33 @@ function covertToStylizedLetters(str: string, offset: { capital: number, small: 
 </script>
 
 <template>
-  <div flex="~ col" justify-start gap-1>
-    <div v-if="!letters" class="title-font" pb-8 text-5xl font-bold>
+  <div flex="~ col justify-start items-center gap-3" my-5>
+    <div v-if="!letters" class="title-font" text-5xl font-bold>
       Stylized Letters
     </div>
     <div sticky top-2>
-      <TheInput
-        v-model="letters"
-        placeholder="Please input letters"
-      />
+      <TheInput v-model="letters" placeholder="Please input letters" />
     </div>
-    <div v-if="letters" class="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
-      <TheOutput
-        v-for="map in offset"
-        :key="map.name"
-        :title="map.name"
-        :content="covertToStylizedLetters(letters, map)"
-      />
-    </div>
+    <view v-if="letters" class="mt-25 sm:mt-5">
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+        <TheOutput
+          v-for="map in offset" :key="map.name" :title="map.name"
+          :content="covertToStylizedLetters(letters, map)"
+        />
+      </div>
+    </view>
     <div v-if="letters" color-gray-500>
       tap block to copy
     </div>
   </div>
-  <div flex="~ col" pt-8 op-10 hover:op-50>
-    <div color-gray-500>
+  <div flex="~ col" relative bottom-1 op-10 hover:op-50>
+    <div color-gray-700>
       References: <a
-        color-blue-500
-        href="https://www.unicode.org/charts/PDF/U1D400.pdf" target="_blank"
+        color-blue-800 href="https://www.unicode.org/charts/PDF/U1D400.pdf"
+        target="_blank"
       >unicode/block/U+1D400</a>
     </div>
-    <div color-gray-500>
+    <div color-gray-700>
       Copyright © 2023 <a color-blue-500 href="https://aidbo.fun" target="_blank">Aidbo Studio</a>. All rights
       reserved.
     </div>
@@ -86,6 +83,6 @@ meta:
 
 .title-font {
   font-family: 'Nunito', sans-serif;
-  color: #5e6a78;
+  color: #bacf65;
 }
 </style>
